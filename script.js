1fc3414ab10e974969d98444db829f8e53983493
Abhi-jith42 <abhijithabhi1073@gmail.com>
2025-07-27 17:31:50 +0530

    adding index and js

---
+++ script.js
@@ -0,0 +1,27 @@
+
+function display(num){
+    result.value+=num
+}
+
+function allclear(){
+    result.value=""
+
+}
+
+function back(){
+    result.value=result.value.slice(0,-1)
+}
+
+function equalto(){
+    try {
+            result.value=eval(result.value)
+
+    } catch  {
+        result.value="error"
+        setTimeout(() => {
+          result.value=""  
+        }, 1000);
+    }
+
+
+}
